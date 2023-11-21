b67f1c712722c3cf424f561114bd344793e802ea
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-21 15:28:06 +0100

    Update navigation logo and company name

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,9 +6,9 @@
     >
       <div class="flex lg:flex-1">
         <RouterLink to="/" class="-m-1.5 p-1.5">
-          <span class="sr-only">Topizy</span>
+          <span class="sr-only">ImmoZero</span>
           <img
-            alt="Topizy logo"
+            alt="ImmoZero logo"
             class="logo"
             src="@/assets/logo.svg"
             height="40"
@@ -78,11 +78,10 @@
       >
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
             <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
+              class="h-20 w-auto"
+              src="@/assets/logo.svg"
+              alt="logo ImmoZero"
             />
           </a>
           <button
@@ -132,70 +131,11 @@
   </header>
 </template>
 
+
+
 <script setup>
 import { ref } from "vue";
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from "@headlessui/vue";
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from "@heroicons/vue/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/vue/20/solid";
-
-const products = [
-  {
-    name: "Analytics",
-    description: "Get a better understanding of your traffic",
-    href: "#",
-    icon: ChartPieIcon,
-  },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers",
-    href: "#",
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: "Security",
-    description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools",
-    href: "#",
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: "Automations",
-    description: "Build strategic funnels that will convert",
-    href: "#",
-    icon: ArrowPathIcon,
-  },
-];
-const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
-
+import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 const mobileMenuOpen = ref(false);
 </script>
