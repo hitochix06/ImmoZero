675b9368726e852e459cb99180524ab543813444
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-21 20:36:10 +0100

    Add "Mots de passe oublié" link in ConnexionView.vue

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -28,6 +28,9 @@
                   class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
                   placeholder="Mots de passe"
                 />
+                <p class="text-right text-sm m-2" style="color: red">
+                  Mots de passe oublié
+                </p>
                 <button
                   type="submit"
                   class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 m-2"
