1ae06dbfd5f42f263611b3931578dda72a89108c
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-21 20:03:50 +0100

    Refactor CSS and update ConnexionView.vue

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -1,11 +1,101 @@
 <template>
-  <div class="fondecran">
-<h1>Connexion</h1>
+  <div class="fondecran h-screen">
+    <section class="flex items-center justify-center h-full">
+      <div class="w-3/4 lg:w-1/2">
+        <div class="flex flex-wrap">
+          <div
+            class="w-full lg:w-1/2 p-4"
+            style="background-color: white; border-radius: 37px 0px 0px 37px"
+          >
+            <div class="flex justify-center">
+              <img
+                alt="ImmoZero logo"
+                class="logo"
+                src="@/assets/logo.svg"
+                height="40"
+                width="133"
+              />
+            </div>
+            <form>
+              <h1 class="mb-8 text-3xl text-center">Se Connecter</h1>
+              <div class="mb-4">
+                <label class="block" for="formEmail">Email</label>
+                <input
+                  type="email"
+                  id="formEmail"
+                  class="form-control"
+                  placeholder="Adresse Email"
+                  required
+                />
+              </div>
+              <div class="mb-4">
+                <label class="block" for="formopasse">mots passe</label>
+                <input
+                  type="password"
+                  id="formopasse"
+                  class="form-control"
+                  required
+                />
+                <a class="text-gray-500" href="#!">Mot de passe oublié ?</a>
+              </div>
+              <div class="text-center pt-1 mb-5 pb-1">
+                <button type="submit" class="btn btn-light btn-lg">
+                  Entre
+                </button>
+              </div>
+              <div class="flex items-center justify-center pb-4">
+                <p class="mb-0 mr-2">Vous n’avez pas de compte ?</p>
+                <a
+                  type="button"
+                  class="btn btn-outline"
+                  href="https://www.gais-nice.org/registration/register.html"
+                  target="_blank"
+                  role="button"
+                  >Créez</a
+                >
+              </div>
+            </form>
+          </div>
+          <div
+            class="w-full lg:w-1/2 flex items-center justify-center text-center text-white p-4"
+            style="background-color: #143457; border-radius: 0px 37px 37px 0px"
+          >
+            <div class="px-3 py-4 mx-4">
+              <h1 class="mb-4 text-3xl">CRÉÉZ VOTRE COMPTE</h1>
+              <h5>
+                Vous souhaitez passer une petite annonce immobilière de
+                particulier à particulier et gratuitement ?
+              </h5>
+              <div class="mt-6 flex flex-col max-w-md gap-x-4">
+                <label for="email-address" class="sr-only">Email address</label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                  placeholder="Adress e-mail"
+                />
+                <button
+                  type="submit"
+                  class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 m-2"
+                >
+                  C'est Parti !
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
-
 <style>
 .fondecran {
   background-color: #e8eef5;
+}
+.couleur_fond {
+  /* background-color: #143457; */
+  /* border-radius: 0px 37px 37px 0px; */
 }
 </style>
