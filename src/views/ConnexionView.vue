8fe2a2bd9a086749be8cb88460fd634379fd5e32
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-22 11:45:23 +0100

    Refactor ConnexionView.vue component

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -5,50 +5,63 @@
         class="mx-auto mt-16 max-w-full rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 flex flex-col lg:flex-row"
       >
         <div
-          class="p-8 sm:p-10 lg:flex-auto flex-1"
-          style="background-color: white; border-radius: 37px 0px 0px 37px"
+          class="-mt-2 p-5 lg:mt-0 lg:w-auto lg:max-w-full lg:flex-shrink-0 h-auto flex-grow lg:flex-grow rounded-l-lg"
+          style="background-color: #ffffff"
         >
-          <h1
-            class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl text-center"
+          <div
+            class="text-center ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16"
           >
-            SE CONNECTER
-          </h1>
-          <form>
-            <div class="mt-6 flex flex-col max-w-md gap-x-4">
-              <label for="email-address"></label>
-              <input
-                id="email_address"
-                name="email"
-                type="email"
-                autocomplete="email"
-                class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
-                placeholder="Adress e-mail"
-              />
-              <label for="passeword"></label>
-              <input
-                id="Mots de passe"
-                name="Mots de passe"
-                type="Mots de passe"
-                autocomplete="Mots de passe"
-                class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
-                placeholder="Mots de passe"
-              />
-              <p class="text-right text-sm m-2" style="color: red">
-                Mots de passe oublié
-              </p>
-              <button
-                type="submit"
-                class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 m-2"
-              >
-                Connexion
-              </button>
-            </div>
-          </form>
+            <form>
+              <div class="mx-auto max-w-xs">
+                <h1
+                  class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl"
+                  style="color: rgb(0, 0, 0); text-transform: uppercase"
+                >
+                  Se connecter
+                </h1>
+
+                <div class="mt-6 flex items-baseline justify-center gap-x-2">
+                  <label for="email-address"></label>
+                  <input
+                    id="email_address"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="E-mai"
+                  />
+                </div>
+                <div class="mt-6 flex items-baseline justify-center gap-x-2">
+                  <label for="password"></label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="Mot de passe"
+                  />
+                </div>
+                <div class="flex justify-end">
+                  <a href="#" class="text-sm m-2" style="color: red">
+                    Mots de passe oublié
+                  </a>
+                </div>
+                <a
+                  href="#"
+                  class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  style="background-color: #43b7be; text-transform: uppercase"
+                  >Connexion
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
+
         <!-- formulaire d'inscription -->
         <div
-          class="-mt-2 p-2 lg:mt-0 lg:w-auto lg:max-w-full lg:flex-shrink-0 h-auto flex-grow lg:flex-grow"
-          style="background-color: #143457; border-radius: 0px 37px 37px 0px"
+          class="-mt-2 p-2 lg:mt-0 lg:w-auto lg:max-w-full lg:flex-shrink-0 h-auto flex-grow lg:flex-grow rounded-r-lg"
+          style="background-color: #143457"
         >
           <div
             class="text-center ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16"
@@ -83,6 +96,7 @@
                 <a
                   href="#"
                   class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  style="text-transform: uppercase; background-color: #43b7be"
                   >C'est Parti !</a
                 >
               </div>
@@ -97,9 +111,6 @@
 <style>
 .fondecran {
   background-color: #e8eef5;
-}
-.bg-indigo-600 {
-  background-color: #43b7be;
 }
 </style>
 
