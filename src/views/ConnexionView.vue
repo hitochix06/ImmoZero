1bab43f78ff4cfc888d23e504519f18e0925547a
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-22 12:15:23 +0100

    Remove AboutView component and update styles in ConnexionView

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -27,7 +27,7 @@
                     name="email"
                     type="email"
                     autocomplete="email"
-                    class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    class="min-w-0 flex-auto rounded-md border-2 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
                     placeholder="E-mai"
                   />
                 </div>
@@ -38,7 +38,7 @@
                     name="password"
                     type="password"
                     autocomplete="current-password"
-                    class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    class="min-w-0 flex-auto rounded-md border-2 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
                     placeholder="Mot de passe"
                   />
                 </div>
---
+++ src/views/UserView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Page de connexion</h1>
+</template>
+
+<style></style>
+
+<script setup></script>
