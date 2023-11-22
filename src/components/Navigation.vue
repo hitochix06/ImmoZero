e728b8c4021e69a3b4927efa7dee84befa5a30c8
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-22 19:20:19 +0100

    correction sur la nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,8 +11,8 @@
             alt="ImmoZero logo"
             class="logo"
             src="@/assets/logo.svg"
-            height="40"
-            width="133"
+            height="70"
+            width="70"
           />
         </RouterLink>
       </div>
@@ -31,22 +31,22 @@
       <div class="hidden lg:flex lg:gap-x-12 texte_majuscule">
         <RouterLink
           to="/"
-          class="text-sm font-semibold leading-6 couleur_fond hover:bg-[#3b5998] focus:ring-4 focus:outline-none focus:ring-[#3b5998]"
+          class="text-sm font-semibold leading-6"
           >Accueil</RouterLink
         >
         <RouterLink
           to="/ventes"
-          class="text-sm font-semibold leading-6 couleur_fond hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50"
+          class="text-sm font-semibold leading-6 "
           >Ventes</RouterLink
         >
         <RouterLink
           to="/apropos"
-          class="text-sm font-semibold leading-6 couleur_fond hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50"
+          class="text-sm font-semibold leading-6 "
           >A propose</RouterLink
         >
         <RouterLink
           to="/contact"
-          class="text-sm font-semibold leading-6 couleur_fond hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50"
+          class="text-sm font-semibold leading-6 "
           >Contact</RouterLink
         >
       </div>
@@ -54,24 +54,24 @@
       <!-- personnaliser icones inscription/connexion -->
 
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <RouterLink to="connexion" class="text-sm font-semibold leading-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-            width="40"
-            height="40"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-              clipRule="evenodd"
-              style="color: #ffffff"
-            />
-          </svg>
-        </RouterLink>
-      </div>
+  <RouterLink to="connexion" class="text-sm font-semibold leading-6 flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="w-6 h-6 mr-2"
+      width="40"
+      height="40"
+    >
+      <path
+        fillRule="evenodd"
+        d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+    Connexion/incrisption
+  </RouterLink>
+</div>
     </nav>
 
     <Dialog
@@ -151,10 +151,7 @@
   border-radius: 10px;
 }
 
-.navigation-header {
-  background-image: url("@/assets/images/fondecran.png");
-  background-size: cover;
-}
+
 </style>
 
 <script setup>
