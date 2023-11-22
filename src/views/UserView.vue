a79bce1085a5d587f7493b21f56e19df52b66f46
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-22 16:36:31 +0100

    Remove User component and add modal form

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -36,6 +36,7 @@
 
           <div class="container mx-auto flex justify-center mt-10">
             <button
+              @click="isOpen = true"
               type="button"
               class="text-white bg-[#43B7BE] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2"
             >
@@ -181,11 +182,63 @@
       </div>
     </div>
   </div>
+
+  <!-- creation formulaire de modal  -->
+
+  <div class="container mx-auto">
+    <div class="flex justify-center">
+      <div
+        v-show="isOpen"
+        class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
+      >
+        <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl">
+          <div class="flex items-center justify-between">
+            <h3 class="text-2xl">Model Title</h3>
+            <svg
+              @click="isOpen = false"
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-8 h-8 text-red-900 cursor-pointer"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+          <div class="mt-4">
+            <p class="mb-4 text-sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus qui nihil laborum quaerat blanditiis nemo explicabo
+              voluptatum ea architecto corporis quo vitae, velit temporibus
+              eaque quisquam in quis provident necessitatibus.
+            </p>
+            <button
+              @click="isOpen = false"
+              class="px-6 py-2 text-blue-800 border border-blue-600 rounded"
+            >
+              Cancel
+            </button>
+            <button class="px-6 py-2 ml-2 text-blue-100 bg-blue-600 rounded">
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+import { ref } from "vue";
+
+const isOpen = ref(false);
 
 const products = [
   {
