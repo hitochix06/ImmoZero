ab657f829de003ba62cdc070d532692d6d0439f4
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-23 22:17:33 +0100

    Refactor UserView component styles

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -189,7 +189,9 @@
         v-show="isOpen"
         class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
       >
-        <div class="w-full md:w-2/3 lg:w-3/5 p-6 bg-white rounded-md shadow-xl">
+        <div
+          class="w-full md:w-2/3 lg:w-3/5 p-6 bg-white rounded-md shadow-xl m-2"
+        >
           <div class="flex items-center justify-between">
             <h3
               class="text-2xl"
