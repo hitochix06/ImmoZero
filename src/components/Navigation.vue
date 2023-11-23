75d14776bde2070799875f449107f5e774484de4
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-23 15:24:43 +0100

    Fix navigation component login/logout functionality

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -57,6 +57,7 @@
 
       <div class="hidden lg:flex lg:flex-1 lg:justify-end text-white">
         <RouterLink
+          v-if="!isUserLoggedIn"
           to="connexion"
           class="text-sm font-semibold leading-6 flex items-center hover:underline"
         >
@@ -74,8 +75,29 @@
               clipRule="evenodd"
             />
           </svg>
-          Connexion/incrisption
+          Connexion/Inscription
         </RouterLink>
+        <button
+          v-else
+          type="button"
+          class="text-sm font-semibold leading-6 flex items-center hover:underline"
+          @click="logout"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
+          Déconnexion
+        </button>
       </div>
     </nav>
 
@@ -163,4 +185,14 @@ import { Dialog, DialogPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 
 const mobileMenuOpen = ref(false);
+const isUserLoggedIn = ref(localStorage.getItem("token") !== null); // Change this to true if a token is found in localStorage
+
+// ajouter une fonction logout
+function logout() {
+  // Remove the user's token from localStorage
+  localStorage.removeItem("token");
+
+  // Set isUserLoggedIn to false
+  isUserLoggedIn.value = false;
+}
 </script>
