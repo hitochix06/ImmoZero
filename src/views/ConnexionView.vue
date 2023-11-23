e587111bb2eb5df1774909108c94d085a8da2cd1
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-23 12:16:30 +0100

    Fix styling and add success alert on account creation

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -147,7 +147,7 @@
               />
             </svg>
           </div>
-          <div class="mt-4 uppercase ">
+          <div class="mt-4 uppercase">
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div class="sm:col-span-3">
                 <div class="mt-2">
@@ -211,6 +211,10 @@
             </button>
             <button
               type="submit"
+              @click="
+                showAlert = true;
+                $router.push('/connexion');
+              "
               class="ml-2 text-[#fff] bg-[#43B7BE] rounded hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
             >
               Ajouter
@@ -218,6 +222,53 @@
           </div>
         </div>
       </form>
+    </div>
+
+    <!-- cree une alerte -->
+    <div
+      v-show="showAlert"
+      id="alert-border-3"
+      class="fixed top-0 right-0 flex items-center p-5 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <div class="ms-3 text-sm font-medium">
+        Votre compte a été créé avec succès !
+        <a href="#" class="font-semibold underline hover:no-underline"></a>
+      </div>
+      <button
+        @click="showAlert = false"
+        type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
+        data-dismiss-target="#alert-border-3"
+        aria-label="Close"
+      >
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -232,10 +283,12 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
-import { useRouter } from 'vue-router';
 
+// Modal
 const isOpen = ref(false);
-const router = useRouter();
+
+// Alert
+const showAlert = ref(false);
 
 // Connexion
 const email_connexion = ref("");
@@ -261,9 +314,7 @@ const handleInscription = async () => {
     if (response.status === 201) {
       const user = await response.json();
       console.log(user);
-      // Stocker le message de succès dans le store Vuex
-      window.alert("Votre compte a été créé avec succès !");
-      window.location.href = "/connexion";
+      showAlert.value = true;
     } else {
       // Gérer l'erreur d'inscription
     }
