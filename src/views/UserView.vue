eac3689e0698cf0dfe673560be3fba116450b624
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-24 12:29:19 +0100

    Update grid layout in UserView.vue

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -150,7 +150,7 @@
 
       <!-- Création de la carte -->
       <div
-        class="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+        class="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-10"
       >
         <div
           v-for="item in data"
@@ -158,52 +158,48 @@
           class="group relative shadow-xl bg-white overflow-hidden rounded-lg"
         >
           <div
-            class="group relative shadow-xl bg-white overflow-hidden rounded-lg"
+            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-60"
           >
-            <div
-              class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-60"
-            >
-              <img
-                src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/295090917.jpg?k=d17621b71b0eaa0c7a37d8d8d02d33896cef75145f61e7d96d296d88375a7d39&o=&hp=1"
-                class="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div class="mt-4 flex justify-between">
-              <div>
-                <h1 class="text-lg text-black-900 m-5">
-                  {{ item.title }}
-                </h1>
-                <p class="mt-1 text-sm text-black-900 m-5">
-                  {{ item.description }}
-                </p>
-                <p class="mt-1 text-sm text-black-900 m-5">
-                  {{ item.location }}
-                </p>
-              </div>
-              <p class="text-lg font-medium m-5 text-gray-900">
-                {{ item.price }}€
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/295090917.jpg?k=d17621b71b0eaa0c7a37d8d8d02d33896cef75145f61e7d96d296d88375a7d39&o=&hp=1"
+              class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h1 class="text-lg text-black-900 m-5">
+                {{ item.title }}
+              </h1>
+              <p class="mt-1 text-sm text-black-900 m-5">
+                {{ item.description }}
               </p>
-
-              <!-- 
-               Création du bouton supprimer -->
-              <button
-                @click="deleteProduct(item.id)"
-                class="absolute top-0 right-0 p-2 text-gray-400 hover:text-red-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
+              <p class="mt-1 text-sm text-black-900 m-5">
+                {{ item.location }}
+              </p>
             </div>
+            <p class="text-lg font-medium m-5 text-gray-900">
+              {{ item.price }}€
+            </p>
+
+            <!-- 
+               Création du bouton supprimer -->
+            <button
+              @click="deleteProduct(item.id)"
+              class="absolute top-0 right-0 p-2 text-gray-400 hover:text-red-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
