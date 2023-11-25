1c627a240f757fa7c469472577bbebc77bdd14b3
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-25 19:25:42 +0100

    Add alert message for successful login

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -272,6 +272,33 @@
     </button> -->
     </div>
   </transition>
+
+  <!-- cree une alerte  connexion-->
+  <transition name="slide-fade">
+    <div
+      v-show="showAlertConnexion"
+      :class="{ 'alert-hide': !showAlertConnexion }"
+      id="alert-border-3"
+      class="fixed top-0 right-0 flex items-center p-5 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <div class="ms-3 text-sm font-medium">
+        tu es connecté !
+        <a href="#" class="font-semibold underline hover:no-underline"></a>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <style scoped>
@@ -303,8 +330,11 @@ const router = useRouter();
 // Modal
 const isOpen = ref(false);
 
-// Alert
+// Alert message inscription
 const showAlert = ref(false);
+
+// Alert message connexion
+const showAlertConnexion = ref(false);
 
 // Connexion
 const email_connexion = ref("");
@@ -357,9 +387,15 @@ const handleConnexion = async () => {
 
     const token = response.data.token;
     localStorage.setItem("token", token);
-    alert("Vous êtes connecté");
-    router.go();
+
+    showAlertConnexion.value = true; // Affichez votre alerte
+    // router.go();
     router.push("/user");
+
+    // Faire disparaître l'alerte après 5 secondes
+    setTimeout(() => {
+      showAlertConnexion.value = false;
+    }, 3000);
   } catch (error) {
     alert("Adresse e-mail ou mot de passe incorrect");
     // Réinitialiser les champs d'input
