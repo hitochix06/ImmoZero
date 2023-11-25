78d86ab919d5ec4c914f19d86bf3052b0ae25d6d
hitochix06 <pachara.philibert@hotmail.fr>
2023-11-25 10:07:34 +0100

    Update UserView.vue with edit form functionality

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -145,6 +145,25 @@
               class="h-full w-full object-cover object-center lg:h-full lg:w-full"
             />
           </div>
+          <button
+            @click="openEditForm(item)"
+            class="absolute top-0 p-2 left-0 text-gray-400 hover:text-green-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-8"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
+              />
+            </svg>
+          </button>
           <div class="mt-4 flex justify-between">
             <div>
               <h1 class="text-lg text-black-900 m-5">
@@ -171,7 +190,7 @@
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-6 h-6"
+                class="w-8 h-8"
               >
                 <path
                   fill-rule="evenodd"
@@ -329,6 +348,150 @@
       </form>
     </div>
   </div>
+
+  <!-- creation mofification modal  -->
+  <div class="container mx-auto">
+    <div class="flex justify-center">
+      <form
+        @submit.prevent="handleMotifAnnonces"
+        v-show="isEditing"
+        class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
+      >
+        <div
+          class="w-full md:w-2/3 lg:w-3/5 p-6 bg-white rounded-md shadow-xl m-2"
+        >
+          <div class="flex items-center justify-between">
+            <h3
+              class="text-2xl"
+              style="text-transform: uppercase; font-weight: 700"
+            >
+              modifier une annonce
+            </h3>
+            <svg
+              @click="isEditing = false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 "
+              width="30"
+              height="30"
+              style="cursor: pointer"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <!-- 
+          <div class="mt-2">
+            <label for="image" class="block text-sm font-medium leading-6 text-gray-900">Image</label>
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              class="min-w-full flex-auto rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+              required
+            />
+          </div> -->
+
+          <div class="mt-4">
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="sm:col-span-3">
+                <div class="mt-2">
+                  <label
+                    for="first-name"
+                    class="block text-sm font-medium leading-6 text-gray-900"
+                    >Titre</label
+                  >
+                  <input
+                    v-model="title"
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autocomplete="given-name"
+                    class="min-w-full flex-auto rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="Titre"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="sm:col-span-3">
+                <div class="mt-2">
+                  <label for="Price">Prix</label>
+                  <input
+                    v-model="price"
+                    type="number"
+                    name="price"
+                    id="price"
+                    autocomplete="off"
+                    class="min-w-full flex-auto rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="Prix €"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="sm:col-span-3">
+                <div class="mt-2">
+                  <label
+                    for="location"
+                    class="block text-sm font-medium leading-6 text-gray-900"
+                    >Emplacement</label
+                  >
+                  <input
+                    v-model="location"
+                    id="location"
+                    name="location"
+                    type="text"
+                    autocomplete="off"
+                    class="min-w-full flex-auto rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="Ville"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="sm:col-span-4">
+                <label
+                  for="description"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                  >Description</label
+                >
+                <div class="mt-2">
+                  <textarea
+                    v-model="description"
+                    id="description"
+                    name="description"
+                    autocomplete="off"
+                    class="min-w-full flex-auto rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 m-2"
+                    placeholder="Description"
+                    required
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+
+            <button
+              @click="isEditing = false"
+              class="px-6 py-2 text-blue-800 border border-blue-600 rounded"
+            >
+              Annuler
+            </button>
+            <button
+              type="submit"
+              class="ml-2 text-[#fff] bg-[#43B7BE] rounded hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
+            >
+              Ajouter
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -349,7 +512,21 @@ const description = ref("");
 const price = ref("");
 const location = ref("");
 const searchValue = ref("");
+const editingId = ref(null);
+const isEditing = ref(false);
 
+const openEditForm = (item) => {
+  isEditing.value = true;
+  title.value = item.title;
+  description.value = item.description;
+  price.value = item.price;
+  location.value = item.location;
+
+  // Stockez l'ID de l'annonce que nous modifions
+  editingId.value = item.id;
+};
+
+// Fonctions recherche
 const search = async () => {
   try {
     const token = localStorage.getItem("token");
@@ -392,7 +569,44 @@ const handleCreationAnnonces = async () => {
     );
     isOpen.value = false;
     router.go();
-    alert("Annonce ajoutée avec succès");
+    alert("Annonce modifiée avec succès");
+
+    // Reset des valeurs
+    title.value = "";
+    description.value = "";
+    price.value = "";
+    location.value = "";
+  } catch (error) {
+    console.error("Erreur lors de la création de l'annonce: ", error);
+    alert("Erreur lors de l'ajout de l'annonce");
+  }
+};
+
+// Fonction modification
+const handleMotifAnnonces = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const url = editingId.value
+      ? `https://apihackaton1.osc-fr1.scalingo.io/update-properties/${editingId.value}`
+      : "https://apihackaton1.osc-fr1.scalingo.io/create-properties";
+    const method = editingId.value ? "put" : "post";
+    const response = await axios[method](
+      url,
+      {
+        title: title.value,
+        description: description.value,
+        price: price.value,
+        location: location.value,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    isEditing.value = false;
+    
+    router.go();
 
     // Reset des valeurs
     title.value = "";
